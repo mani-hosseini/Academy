79f1b55ba1f1e2795ddc8701106d8b485f671c13
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-14 00:43:19 +0330

    respansiv baner

--- page2/src/components/baner/baner-components/rightbaner.jsx
+++ page2/src/components/baner/baner-components/rightbaner.jsx
@@ -1,12 +1,12 @@
 export function Rightbaner() {
     return (
-        <div className={'flex items-center justify-between child:w-1/2 h-[468px]'}>
-            <div>
+        <div className={'lg:flex items-center lg:mt-0 sm:mt-[30px] justify-between lg:child:w-1/2 lg:h-[468px] '}>
+            <div className={'flex flex-col sm:items-center lg:items-start sm:justify-center lg:justify-between'}>
                 <a href="" className={'px-[20px] py-[10px] border-[2px]  border-black rounded-[8px]'}>Academics</a>
-                <h1 className={'text-[48px] mt-4 font-bold'}>Nurturing Young Minds for Success</h1>
+                <h1 className={'lg:text-[48px] sm:text-[28px] mt-4 font-bold'}>Nurturing Young Minds for Success</h1>
             </div>
             <div>
-                <p className={'text-[20px] text-[#333333]'}>Welcome to our Academics page, where we take pride in providing a comprehensive and stimulating
+                <p className={'text-[20px] text-[#333333] lg:mt-0 sm:text-center lg:text-justify sm:mt-[30px]'}>Welcome to our Academics page, where we take pride in providing a comprehensive and stimulating
                     educational experience for your child. Our kindergarten school's academic program is thoughtfully
                     designed to foster a love for learning while building a strong foundation of essential skills and
                     knowledge. From language arts and mathematics to science and social studies, our curriculum is
--- page2/src/components/baner/baner.jsx
+++ page2/src/components/baner/baner.jsx
@@ -2,8 +2,8 @@ import {Rightbaner} from "./baner-components/rightbaner.jsx";
 
 export function Baner() {
     return (
-        <div className={'lg:flex sm:block  items-center custom-box px-[70px] relative'}>
-            <img src="../../../src/assets/baner/Abstract%20Design.png" className={'absolute top-0 left-0'} alt=""/>
+        <div className={'lg:flex   items-center custom-box lg:px-[70px] relative'}>
+            <img src="../../../src/assets/baner/Abstract%20Design.png" className={'absolute top-0 left-0 sm:w-[100px] sm:h-[100px] lg:w-[200px] lg:h-[200px]' } alt=""/>
             <Rightbaner/>
         </div>
     );
